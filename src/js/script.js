ab63c71271030e5373688326ca7843d016f6a916
17Sensei09 <thasensei95@gmail.com>
2024-01-07 13:06:54 +0200

    Homework 3 completed

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const number1 = parseFloat((prompt('Enter first number')));
+
+const number2 = parseFloat((prompt('Enter second number')));
+
+console.log('addition result'  + number1 + '+' + number2 + '=' +(number1+number2));
+
+console.log('substraction result'  + number1 + '-' + number2 + '=' +(number1-number2));
+
+console.log('multiplication result'  + number1 + '*' + number2 + '=' +(number1*number2));
+
+console.log('division result'  + number1 + '/' + number2 + '=' +(number1/number2));
