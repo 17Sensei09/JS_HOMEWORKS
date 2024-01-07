49c237133b928eb5340d38ea89b86716d44a7c69
17Sensei09 <thasensei95@gmail.com>
2024-01-07 12:37:07 +0200

    Homework 2 completed

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+'use strict'
+//task 1
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+//task 2
+let PlanetName;
+let userName;
+
+//task 3
+let CheckOut = ('10') +10
+console.log(CheckOut)
