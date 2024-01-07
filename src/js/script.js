697093e6e94a8b7f7205cc4fe49f2977f7ba81e6
17Sensei09 <thasensei95@gmail.com>
2024-01-07 12:39:40 +0200

    Homework 1 upd.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
 'use strict'
-    const userName= prompt('Enter your name');
+const userName= prompt('Enter your name');
 const userSurname= prompt('Enter your surname ')
 alert('Hello, '+ userName + ' ' + userSurname +', how are you?');
