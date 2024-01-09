4332f84d12798889b808b60f71c5fa3125c77822
17Sensei09 <thasensei95@gmail.com>
2024-01-09 23:44:07 +0200

    HW13 completed

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+//с помощью дополнительных инструментов решил поиграться чтоб при вводе неверного значения окно не просто закрывалось
+//а закидывало обратно на начальный вопрос, в целом принцип работы функций калькулятора уловил, c while еще разберусь
+//+ поменял математические функции на фактические их символы для вывода в алерт результатов
+let question;
+let isValidOperation = false;
+
+while (!isValidOperation) {
+    question = prompt('Hello, what do you want to do? (add, div, mult, diff)');
+
+    switch (question) {
+        case 'add':
+        case 'diff':
+        case 'mult':
+        case 'div':
+            isValidOperation = true;
+            break;
+        default:
+            alert('Entered wrong operation, please choose one of the given');
+    }
+}
+let num1 = prompt('Enter first value');
+let num2 = prompt('Enter second value')
+//в этой домашке, как и 12 не получилось реализовать через константы т.к. редактор выдает ошибку
+//по быстрым подсказкам редактор предложил заменить на let, как я понял это связано с тем, что мы будем получать
+//динамические значения каждый раз при запросе, а не статические, поправь если не правильно понял
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let calculation;
+let calculationSymbol;
+switch (question) {
+    case 'add':
+        calculation = num1 + num2;
+        calculationSymbol = '+';
+    break;
+
+    case 'diff':
+        calculation = num1 - num2;
+        calculationSymbol = '-';
+    break;
+
+    case 'mult':
+        calculation = num1 * num2;
+        calculationSymbol = '*';
+    break;
+
+    case 'div':
+        calculation = num1 / num2;
+        calculationSymbol = '/';
+    break;
+}
+
+let Results = `${num1} ${calculationSymbol} ${num2} = ${calculation}`;
+alert(Results);
+console.log(calculation);
+console.log(Results);
+
+
