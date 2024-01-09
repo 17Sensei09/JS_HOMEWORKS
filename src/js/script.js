b0d1722c7d127d58b2fe8e04368c171646d90f02
17Sensei09 <thasensei95@gmail.com>
2024-01-10 00:13:46 +0200

    HW15 completed

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let num1 = parseFloat(prompt('Enter first value'));
+let num2 = parseFloat(prompt('Enter second value'));
+let num3 = parseFloat(prompt('Enter third value'));
+
+let average;
+average = (num1 + num2 + num3)/3;
+console.log(average);
+alert('Your average value:' + average);
