ff74b5e78f593b19e39eaf691c14cea03b8a1710
17Sensei09 <thasensei95@gmail.com>
2024-01-10 22:08:19 +0200

    HW26 completed

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const userNum = +prompt('Enter your number');
+
+if(isNaN(userNum) || userNum < 1){
+    console.log(NaN)
+}else{
+    let divisor = 2;
+    while(divisor <= userNum){
+        if(userNum % 2 === 0){
+            console.log('Lowest divisor:', divisor);
+            break;
+        }
+        divisor++
+    }
+    if (divisor > userNum){
+        console.log('Your number is divisor')
+    }
+
+}
