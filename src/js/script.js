f764ce7d3a87189a9730aa9e06772e3889da9701
17Sensei09 <thasensei95@gmail.com>
2024-01-10 18:14:15 +0200

    HW14 constant updates

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,7 @@
 'use strict'
 
-let question = prompt('Hello, how many hours do you want to convert?');
-parseFloat(question);
-let hours_to_seconds;
-hours_to_seconds = question * 3600;
+const question = +prompt('Hello, how many hours do you want to convert?');
+const hours_to_seconds = question * 3600;
 alert('Convertation result:'+ hours_to_seconds);
 console.log(question);
 console.log('Convertation result:',hours_to_seconds);
