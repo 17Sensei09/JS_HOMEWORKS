481babf5a7a3039d52ec6ec51dc5ba1ae22345e5
17Sensei09 <thasensei95@gmail.com>
2024-01-10 18:05:48 +0200

    HW15 constant updates

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let num1 = parseFloat(prompt('Enter first value'));
-let num2 = parseFloat(prompt('Enter second value'));
-let num3 = parseFloat(prompt('Enter third value'));
+const num1 = parseFloat(prompt('Enter first value'));
+const num2 = parseFloat(prompt('Enter second value'));
+const num3 = parseFloat(prompt('Enter third value'));
 
-let average;
-average = (num1 + num2 + num3)/3;
+
+const average = (num1 + num2 + num3)/3;
 console.log(average);
 alert('Your average value:' + average);
